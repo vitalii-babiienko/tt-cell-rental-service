87049be273f0f1f9624a14471deb8514c235f8ab
Vitalii Babiienko <v.babiyenko@gmail.com>
2024-02-05 09:19:10 +0200

    Improve project security with python-dotenv library

--- cell_rental_service/settings.py
+++ cell_rental_service/settings.py
@@ -1,4 +1,9 @@
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@(z&w$g8&k&bmkp5$!vs-q%g_e5bxj4hwsqsgfl6z_ww+6!%4h"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = []
 
