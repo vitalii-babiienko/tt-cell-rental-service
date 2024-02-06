4530258edca4413f0cb2e98eecad1b2af469c9bc
Vitalii Babiienko <v.babiyenko@gmail.com>
2024-02-06 10:34:43 +0200

    Add path for the 'api/v1/orders/' endpoint

--- order/urls.py
+++ order/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from order.views import OrderCreateView
+
+urlpatterns = [
+    path("api/v1/orders/", OrderCreateView.as_view(), name="order_create_api")
+]
 
 app_name = "order"
