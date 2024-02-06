4544390ec197f8632c9cd44d75ff689df65f3700
Vitalii Babiienko <v.babiyenko@gmail.com>
2024-02-06 09:34:08 +0200

    Add requests to the requirements.txt

--- cell_rental_service/urls.py
+++ cell_rental_service/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/", include("order.urls", namespace="order"))
+    path("", include("order.urls", namespace="order"))
 ]
